efdbaa9a3943b144b723e4a6d287df22c009720b
Anastasiia Kushnir <anastasia.kushnir@icloud.com>
2022-06-20 23:57:19 +0200

    adding Console Application

---
+++ StaryLev/ConsoleApp1/BlobCacheService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiServices.Models;
+using System.Reactive.Linq;
+using Akavache;
+
+namespace ConsoleApp1
+{
+    public class BlobCacheService
+    {
+        private const string applicationName = "BookBlobStorage";
+        public BlobCacheService()
+        {
+            Registrations.Start(applicationName);
+        }
+
+        public async Task CacheCurrentCollection(List<Book> books, string index)
+        {
+            await BlobCache.UserAccount.InsertObject(index,books);
+        }
+
+        public async Task<List<Book>> GetCollection(string index)
+        {
+            return await BlobCache.UserAccount.GetObject<List<Book>>(index);
+        }
+    }
+}
---
+++ StaryLev/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using ApiServices.Services;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            //var password = "password";
+            //var nickname = "NastiaRoma";
+
+            //var graphQLClient = new GraphQLHttpClient("https://localhost:44306/graphql/", new NewtonsoftJsonSerializer());
+
+            //var query = "query($password: String  $nickname: String){logIn(dto: { nickname: $nickname, password: $password}){ id nickname roles token  }}";
+
+
+            //var heroRequest = new GraphQLRequest
+            //{
+            //    Query = query,
+            //    Variables = new
+            //    {
+            //        password = password,
+            //        nickname = nickname
+
+            //    }
+            //};
+
+            ////var heroRequest = RequestBuilder.BuildRequest(RequstType.Query);
+
+
+            //var graphQLResponse = await graphQLClient.SendQueryAsync<RensponseLoginMessage>(heroRequest);
+
+            BlobCacheService blobCacheService = new BlobCacheService();
+
+            UserServices userServices = new UserServices(@"https://localhost:5000/graphql/");
+
+            BookApiServices bookServices = new BookApiServices(@"https://localhost:5000/graphql/");
+
+            var list = await bookServices.GetAllBookAsync(1, 5);
+
+            await blobCacheService.CacheCurrentCollection(list, "1");
+
+            var cacheList = await blobCacheService.GetCollection("1");
+
+            foreach(var book in cacheList)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            var user = await userServices.Login("romka", "romka");
+
+            //var res = await bookServices.LikeBookAsync("624dce1f896cf2b11a0f2d01", user.Token);
+
+            //var res = await bookServices.GetAllBook(3, 5);
+
+
+
+            //Console.WriteLine(res.LikeUserNicknames.Count);
+            //Console.WriteLine(res.LikeUserNicknames[0]);
+            //Console.WriteLine(res[0].Country);
+            //Console.WriteLine(graphQLResponse.Data[0].Title.ToString());
+            //Console.WriteLine(graphQLResponse.Data[0].Title);
+
+
+            Console.ReadLine();
+        }
+    }
+}
