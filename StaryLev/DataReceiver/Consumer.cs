00dac42fb4e38a81f985b7187eec67a6f365b156
Anastasiia Kushnir <anastasia.kushnir@icloud.com>
2022-06-20 23:48:27 +0200

    adding functionality for data receiver

---
+++ StaryLev/DataReceiver/Consumer.cs
@@ -0,0 +1,50 @@
+﻿using MongoDb.Models;
+using MongoDb.repository;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace DataReceiver
+{
+    public class Consumer
+    {
+        readonly IRepository<Book> _repository = new Repository<Book>();
+        readonly ConnectionFactory _factory = new ConnectionFactory() { HostName = "localhost" };
+
+        public void Start(CancellationToken token)
+        {
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+               queue: "book_queue",
+               autoDelete: false,
+               exclusive: false,
+               durable: false,
+               arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += async (sender, e) =>
+            {
+                var body = e.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+                var book = JsonSerializer.Deserialize<Book>(message);
+                Console.WriteLine(" Received book: \"{0}\"", book.Title);
+                await _repository.InsertOneAsync(book);
+            };
+
+            channel.BasicConsume(queue: "book_queue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine($"Subscribed to the queue 'book_queue'");
+
+            token.WaitHandle.WaitOne();
+            Console.WriteLine($"Cancellation is requested");
+        }
+    }
+}
---
+++ StaryLev/DataReceiver/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Consumer consumer = new Consumer();
+            Task.Run(()=> consumer.Start(token));
+            
+            Console.ReadLine();
+            cts.Cancel();
+        }
+    }
+}
